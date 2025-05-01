cf4ff8b2b905ddcdfc15ba2906239e6e2a7abc88
nrbnayon <nrbnayon@gmail.com>
2025-05-01 12:04:28 +0600

    Enable state handling for PKCE in Microsoft authentication strategy

--- config/passport.js
+++ config/passport.js
@@ -239,7 +239,7 @@ const strategies = {
       ],
       tenant: "common",
       pkce: true,
-      state: false, // Added to enable state handling with PKCE
+      state: true, // Added to enable state handling with PKCE
     },
     Strategy: MicrosoftStrategy,
   },
